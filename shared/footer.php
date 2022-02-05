0c5a0212e1f52f95fdc406e349c3d0c049c889db
siddharthmishra95 <siiid.mishra@gmail.com>
2022-02-05 14:18:51 +0530

    first

---
+++ shared/footer.php
@@ -0,0 +1,13 @@
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/vendors/js/vendor.bundle.base.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/vendors/chart.js/Chart.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/jquery.cookie.js" type="text/javascript"></script>
+<script src="<?php echo BASE_URL; ?>assets/vendors/js/vendor.bundle.base.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/off-canvas.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/hoverable-collapse.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/misc.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/dashboard.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/js/todolist.js"></script>
+</body>
+
+</html>
